f495ef4e3124279c8daf61cd3abb39441a4baadc
Sangram <sangram15.behera@gmail.com>
2022-07-08 02:15:13 +0530

    Add Employee without thread file

---
+++ EmployeePayrollUsingThread/EmployeePayrollOperations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollUsingThread
+{
+    public class EmployeePayrollOperations
+    {
+        List<EmployeeDetails> employeePayrollDetailList = new List<EmployeeDetails>();
+
+        public void addEmployeeToPayroll(List<EmployeeDetails> employeePayrollDataList)
+        {
+            employeePayrollDataList.ForEach(employeeData =>
+            {
+
+                Console.WriteLine("Employee being added: " + employeeData.EmployeeName);
+                this.addEmployeePayroll(employeeData);
+                Console.WriteLine("Employee Added: " + employeeData.EmployeeName);
+            });
+
+            Console.WriteLine(this.employeePayrollDetailList.ToString());
+        }
+
+        public void addEmployeePayroll(EmployeeDetails emp)
+        {
+            employeePayrollDetailList.Add(emp);
+        }
+
+    }
+}
--- EmployeePayrollUsingThread/Program.cs
+++ EmployeePayrollUsingThread/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome To Employee Payroll Using Thread Problem");
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace EmployeePayrollUsingThread
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Payroll Using Thread Problem");
+            List<EmployeeDetails> employeeDetails = new List<EmployeeDetails>();
+            EmployeePayrollOperations employeePayrollOperations = new EmployeePayrollOperations();
+            DateTime startDateTime = DateTime.Now;
+            employeePayrollOperations.addEmployeeToPayroll(employeeDetails);
+            DateTime stopDateTime = DateTime.Now;
+            Console.WriteLine("Duration without thread: " + (stopDateTime - startDateTime));
+
+        }
+    }
+}
